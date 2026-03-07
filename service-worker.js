86fff383b1a5d0919f69004b6303ac62923450e9
krusureya-jpg <krusureya@kabin.ac.th>
2026-03-07 21:04:35 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,21 @@
-const CACHE_NAME = "mathgame-v1";
+const CACHE="mathgame-v1"
 
-const urlsToCache = [
+self.addEventListener("install",e=>{
+e.waitUntil(
+caches.open(CACHE).then(cache=>{
+return cache.addAll([
 "./",
 "./index.html",
 "./manifest.json"
-];
+])
+})
+)
+})
 
-self.addEventListener("install", event => {
-event.waitUntil(
-caches.open(CACHE_NAME)
-.then(cache => cache.addAll(urlsToCache))
-);
-});
-
-self.addEventListener("fetch", event => {
-event.respondWith(
-caches.match(event.request)
-.then(response => response || fetch(event.request))
-);
-});
+self.addEventListener("fetch",e=>{
+e.respondWith(
+caches.match(e.request).then(r=>{
+return r||fetch(e.request)
+})
+)
+})
